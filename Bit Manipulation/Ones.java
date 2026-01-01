7df50de706aac8f848746d96afea4204865e751e
Kapil <kapily1011@gmail.com>
2026-01-01 09:56:35 +0530

    bit manipulation

---
+++ Bit Manipulation/Ones.java
@@ -0,0 +1,11 @@
+public class Ones {
+    public static void main(String[] args) {
+        System.out.println((~0));
+        System.out.println((~5));
+    }
+    
+}
+
+
+//Java mein ~ operator lagane par minus (-) number kyun aata hai, ye Java ke two's complement system ki wajah se hota hai
+// two's complement mein store karta hai, jahaan sign bit (MSB) decide karta hai positive ya negative
--- Strings/Basics/TypeCasting.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class TypeCasting{
-    public static void main(String args []){
-       
-        float a = 12.36f;
-        int b = (int) a;System.out.println(b);
-    }
-}
